4d636a3bbd3204368c85d880ce25f5dd48deb61b
marton bognar <marton.bognar@cs.kuleuven.be>
2022-06-14 01:29:52 +0200

    Minor fix

--- _exercises/functions-stack/warmup5.asm
+++ _exercises/functions-stack/warmup5.asm
@@ -5,23 +5,23 @@
 # We can just use the stack pointer.
 .text
 main:
-	# load the stack pointer
-	# sp already points to the stack! Nothing to do.
+    # load the stack pointer
+    # sp already points to the stack! Nothing to do.
 
-	# load the integers
-	li t1, 4
-	li t2, 5
+    # load the integers
+    li t1, 4
+    li t2, 5
 
-	# store the integers
-	# WARNING! We are using the stack pointer here!
-	# This means that we need to make space on the stack
-	#  BEFORE we store the integers.
-	addi sp, sp, -8 # Stack grows downwards
-	sw t1, 0(sp)
-	sw t2, 4(sp)
+    # store the integers
+    # WARNING! We are using the stack pointer here!
+    # This means that we need to make space on the stack
+    #  BEFORE we store the integers.
+    addi sp, sp, -8 # Stack grows downwards
+    sw t1, 0(sp)
+    sw t2, 4(sp)
 
-	# load the integers again
-	lw t2, 0(sp)
-	addi sp, sp, 4 # Stack grows downwards and we move it after we loaded the integers
-	lw t1, 0(sp)
-	addi sp, sp, 4
+    # load the integers again
+    lw t1, 0(sp)
+    addi sp, sp, 4 # Stack grows downwards and we move it after we loaded the integers
+    lw t2, 0(sp)
+    addi sp, sp, 4
