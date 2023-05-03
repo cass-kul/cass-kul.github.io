a6b2615e75121f1c91de079ae2b5240b6ab61d8c
marton bognar <marton.bognar@cs.kuleuven.be>
2023-05-03 10:41:26 +0200

    Fix stack pointer convention in session 4

--- _exercises/4-dynamic-memory/ex3.asm
+++ _exercises/4-dynamic-memory/ex3.asm
@@ -18,37 +18,37 @@ stack_pop:      #TODO
 main:
     la s9, heap
 
-    #Test code
-    jal stack_create    #Create stack -> top pointer in a0
+    # Test code
+    jal stack_create    # Create stack -> top pointer in a0
 
     addi sp, sp, -4
-    sw a0, 4(sp)	   #Push top pointer on call stack
+    sw a0, (sp)	        # Push top pointer on call stack
 
-    #Push 1 to stack
+    # Push 1 to stack
     li a1, 1
     jal stack_push
 
-    #Push 2 to stack
-    lw a0, 4(sp)
+    # Push 2 to stack
+    lw a0, (sp)
     li a1, 2
     jal stack_push
 
-    #Push 3 to stack
-    lw a0, 4(sp)
+    # Push 3 to stack
+    lw a0, (sp)
     li a1, 3
     jal stack_push
 
-    #Verify the state of the stack using the single step function!
-    #It should look like the picture below.
+    # Verify the state of the stack using the single step function!
+    # It should look like the picture below.
 
-    #Pop 3 from stack
-    lw a0, 4(sp)
+    # Pop 3 from stack
+    lw a0, (sp)
     jal stack_pop
 
-    #Pop 2 from stack
-    lw a0, 4(sp)
+    # Pop 2 from stack
+    lw a0, (sp)
     jal stack_pop
 
-    #Verify that a0 equals 2 now
-    #Verify that the stack top now points to the value 1
+    # Verify that a0 equals 2 now
+    # Verify that the stack top now points to the value 1
     addi sp, sp, 4
--- _exercises/4-dynamic-memory/sol3.asm
+++ _exercises/4-dynamic-memory/sol3.asm
@@ -14,15 +14,15 @@ stack_create:
 	sw ra, (sp)
 	li a0, 4
 	jal allocate_space
-	
+
 	sw zero, (a0)		#Don't assume allocated memory is zero
 				#For this simple allocator this is the case,
 				#but more complex allocators might break this assumption
-					
+
 	lw ra, (sp)
 	addi sp, sp, 4
 	ret
-	
+
 #Stack push should replace top (in a0) with a new address
 #The new element should point to the old top
 stack_push:
@@ -30,21 +30,21 @@ stack_push:
 	sw ra, (sp)
 	sw a0, 4(sp)		#Back up the top address on the stack
 	sw a1, 8(sp)		#Back up the value-to-push
-	
+
 	li a0, 8			#Allocate space for 2 words
 	jal allocate_space              #New node address in a0
-	
+
 	lw t0, 4(sp)		#Address of top poiner in t0
-	
+
 	#Modify top
 	lw t1, (t0)		#Load old top address in t1
 	sw a0, (t0)		#Store new node address as new top value
-	
+
 	#Init node
 	sw t1, 4(a0)		#Store old top address in new node pointer
 	lw t1, 8(sp)		#Load value-to-push in t1
 	sw t1, (a0)		#Store value-to-push in new node
-	
+
 	lw ra, (sp)
 	addi sp, sp, 12
 	ret
@@ -58,42 +58,42 @@ stack_pop:
 
 main:
     la s9, heap
-  
+
   	#Test code
     jal stack_create    #Create stack -> top pointer in a0
-    
+
     addi sp, sp, -4
-    sw a0, 4(sp)	   #Push top pointer on call stack
-    
+    sw a0, (sp)	   #Push top pointer on call stack
+
     #Push 1 to stack
     li a1, 1
     jal stack_push
-    
+
     #Push 2 to stack
-    lw a0, 4(sp)
+    lw a0, (sp)
     li a1, 2
     jal stack_push
-    
+
     #Push 3 to stack
-    lw a0, 4(sp)
+    lw a0, (sp)
     li a1, 3
     jal stack_push
-    
+
     #Verify the state of the stack using the single step function!
     #At this point, the top pointer should point to the value 3.
     #The next word in memory is a pointer to the value 2.
     #At that location, 2 is stored.
     #The next word in memory is then a pointer to the value 1.
     #The next word in memory after value 1 should be the value 0.
-    
+
     #Pop 3 from stack
-    lw a0, 4(sp)
+    lw a0, (sp)
     jal stack_pop
-    
+
     #Pop 2 from stack
-    lw a0, 4(sp)
+    lw a0, (sp)
     jal stack_pop
-    
+
     #Verify that a0 equals 2 now
     #Verify that the stack top now points to the value 1
     addi sp, sp, 4
